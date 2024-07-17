2d2121382dd4dd5677e9f357b6cc41f7cdcfba02
Eren Başaran <iamerenbasaran@gmail.com>
2024-07-17 20:07:18 +0300

    Name update

---
+++ 4-arrays.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Sample4</title>
+</head>
+<body>
+
+<?php
+
+$trial="Merhaba PHP";
+$trial2="Eren BAŞARAN";
+$trial3="Merhaba CANIM";
+echo $trial." ".$trial2." ".$trial3."<br>";
+
+$array = array("php"=>"Merhaba PHP","eren"=>"Eren BAŞARAN","canim"=>"Merhaba CANIM"); /*1. yol*/
+$array2[] = "Merhaba"; /*2. yol*/
+echo $array["php"]." ".$array["eren"]." ".$array["canim"]."<br>";
+
+?>
+
+</body>
+</html>
